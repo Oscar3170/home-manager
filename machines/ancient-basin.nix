70eeee25d218841ac9d3d12f3e6416596f9fc208
Oscar Lenzi <oscar.lenzi@senior.com.br>
2023-11-16 15:00:07 -0300

    new machine

---
+++ machines/ancient-basin.nix
@@ -0,0 +1,15 @@
+{ home, config, pkgs, ... }:
+{
+    home.username = "oscar";
+    home.homeDirectory = "/home/oscar";
+
+    targets.genericLinux.enable = true;
+
+    home.packages = with pkgs; [
+        kops
+    ];
+    imports = [
+        ./../modules/kubernetes.nix
+        ./../modules/librewolf.nix
+    ];
+}
