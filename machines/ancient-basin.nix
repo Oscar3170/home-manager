776cfdd865c576b62363f419b953869583ff94aa
Oscar Lenzi <oscar.lenzi@senior.com.br>
2024-02-09 07:54:40 -0300

    Format files

--- machines/ancient-basin.nix
+++ machines/ancient-basin.nix
@@ -1,16 +1,18 @@
 { home, config, pkgs, ... }:
 {
-    home.username = "oscar";
-    home.homeDirectory = "/home/oscar";
+  home.username = "oscar";
+  home.homeDirectory = "/home/oscar";
 
-    targets.genericLinux.enable = true;
+  targets.genericLinux.enable = true;
 
-    home.packages = with pkgs; [
-        kops
-    ];
-    imports = [
-        ./../modules/kubernetes.nix
-        ./../modules/librewolf.nix
-        ./../modules/gnome.nix
-    ];
+  home.packages = with pkgs; [
+    kops
+  ];
+  imports = [
+    ./../modules/kubernetes.nix
+    ./../modules/librewolf.nix
+    ./../modules/gnome.nix
+    ./../modules/podman
+    ./ancient-basin.private.nix
+  ];
 }
