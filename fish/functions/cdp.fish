5bc2e10a0213fecc66b25889a0dc1e52dc75e8e9
Oscar Lenzi <oscar.lenzi@terceirizado.credisis.com.br>
2024-11-21 10:38:05 -0300

    fix(fish): func cdp more depth for searching projects

--- fish/functions/cdp.fish
+++ fish/functions/cdp.fish
@@ -1,7 +1,7 @@
 function cdp 
     set -l DIRS ~/Documents ~/scripts
     set -l excludes -E '\.terraform' -E '/\.*/' -E '/go/' -E 'teams-for-linux'
-    set -l selected (fd .git $DIRS --no-ignore-vcs --glob --hidden --prune -d 3 --follow $excludes | sed -e 's/\.git\///g' | fzf)
+    set -l selected (fd .git $DIRS --no-ignore-vcs --glob --hidden --prune -d 5 --follow $excludes | sed -e 's/\.git\///g' | fzf)
 
     if [ -z $selected ]
         return
