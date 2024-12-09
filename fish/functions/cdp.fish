04de15f56fb59d15c46ed24ff9dcac41d6ca98ca
Oscar Lenzi <oscar@oskb.com.br>
2024-12-09 12:30:15 -0300

    feat(fish): added variable cutomization for cdp function

--- fish/functions/cdp.fish
+++ fish/functions/cdp.fish
@@ -1,7 +1,18 @@
 function cdp 
-    set -l DIRS ~/Documents ~/scripts
-    set -l excludes -E '\.terraform' -E '/\.*/' -E '/go/' -E 'teams-for-linux'
-    set -l selected (fd .git $DIRS --no-ignore-vcs --glob --hidden --prune -d 5 --follow $excludes | sed -e 's/\.git\///g' | fzf)
+    set -f fd_args --no-ignore-vcs --glob --hidden --prune --follow
+
+
+    set -q CDP_DEPTH; or set CDP_DEPTH 6
+    set -a fd_args --max-depth $CDP_DEPTH
+
+    set -q CDP_EXCLUDES; or set CDP_EXCLUDES '\.terraform' '/\.*/' '/go/' 'teams-for-linux'
+    for exclude in $CDP_EXCLUDES
+        set -a fd_args -E "$exclude"
+    end
+
+    set -q CDP_DIRS; or set CDP_DIRS ~/Documents ~/scripts
+
+    set -f selected (fd .git $CDP_DIRS $fd_args | sed -e 's/\.git\///g' | fzf)
 
     if [ -z $selected ]
         return
