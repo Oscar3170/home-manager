e522aaa340d4ea1bccb52cd359e7de2f97de868c
Oscar Lenzi <osclen@pm.me>
2025-02-13 15:17:02 -0300

    feat(machines): added new machine crystal-peaks

---
+++ machines/crystal-peaks.nix
@@ -0,0 +1,15 @@
+{ home, config, pkgs, ... }:
+{
+  home.username = "oscar";
+  home.homeDirectory = "/home/oscar";
+
+  targets.genericLinux.enable = true;
+
+  imports = [
+    ../modules/kubernetes
+    # ../modules/librewolf
+    # ../modules/gnome.nix
+    # ../modules/podman
+    ../modules/terraform
+  ];
+}
