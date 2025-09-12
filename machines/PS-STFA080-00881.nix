963e06c3d2f22235e417cf021b706a5db982969b
Oscar Lenzi <oscar.lenzi@terceirizado.credisis.com.br>
2025-09-12 12:11:37 -0300

    feat(machines,PS): launchAgent para keymapping

--- machines/PS-STFA080-00881.nix
+++ machines/PS-STFA080-00881.nix
@@ -9,7 +9,7 @@
   home.packages = with pkgs; [
     gnumake
     gnugrep
-    darwin.iproute2mac
+    iproute2mac
     watch
     openconnect
     wget
@@ -23,6 +23,7 @@
   programs.fish.shellAliases = {
       ip = lib.mkForce "/usr/bin/env ip";
       nc = "ncat";
+      glab = "op plugin run -- glab";
   };
 
   # programs.kitty.enable = true;
@@ -33,6 +34,49 @@
   ];
 
   xdg.mimeApps.defaultApplications = null;
+
+  home.file."Library/LaunchAgents/com.local.KeyRemapping.plist".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>com.local.KeyRemapping</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>/usr/bin/hidutil</string>
+            <string>property</string>
+            <string>--set</string>
+            <!-- 
+              caps_lock       -> esc
+              fn              -> left_control
+              right_option    -> fn
+              right_command   -> right_option 
+            -->
+            <string>{"UserKeyMapping":[
+                {
+                  "HIDKeyboardModifierMappingSrc": 0x700000039,
+                  "HIDKeyboardModifierMappingDst": 0x700000029
+                },
+                {
+                  "HIDKeyboardModifierMappingSrc": 0xFF00000003,
+                  "HIDKeyboardModifierMappingDst": 0x7000000E0
+                },
+                {
+                  "HIDKeyboardModifierMappingSrc": 0x7000000E6,
+                  "HIDKeyboardModifierMappingDst": 0xFF00000003
+                },
+                {
+                  "HIDKeyboardModifierMappingSrc": 0x7000000E7,
+                  "HIDKeyboardModifierMappingDst": 0x7000000E6
+                }
+            ]}</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+    </plist>
+  '';
 
   imports = [
     ../modules/kubernetes
